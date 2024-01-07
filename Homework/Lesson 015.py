8a47508fa906192da9c0c286e8dcbdbd3d459d30
Alex_new_file <zlobzajac84@gmail.com>
2024-01-08 01:33:51 +0500

    master

---
+++ Homework/Lesson 015.py
@@ -0,0 +1,6 @@
+
+print("Площадь окружности радиуса 2:", (lambda pi, radius: pi * (radius ** 2))(3.14, 2))
+print()
+print("Площадь прямоугольника 10*13:", (lambda x, y: x * y)(10, 13))
+print()
+print("Площадь трапеций для a=7, b=5, h=3:", (lambda a, b, h: (a + b) / 2 * h)(7, 5, 3))
